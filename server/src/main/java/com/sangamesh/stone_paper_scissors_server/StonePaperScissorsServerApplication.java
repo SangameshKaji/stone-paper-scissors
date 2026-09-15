bbbd932edbcf9f23c3cf5f908f382480402a69a9
Sangamesh Kaji <sangameshkaji10@gmail.com>
2026-09-15 23:10:08 +0530

    Work starts

---
+++ server/src/main/java/com/sangamesh/stone_paper_scissors_server/StonePaperScissorsServerApplication.java
@@ -0,0 +1,13 @@
+package com.sangamesh.stone_paper_scissors_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StonePaperScissorsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StonePaperScissorsServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/sangamesh/stone_paper_scissors_server/StonePaperScissorsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangamesh.stone_paper_scissors_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StonePaperScissorsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
